679ad07656157c3c22681509abaed8824571a3d9
Yura <Yura>
2021-03-24 21:31:15 +0200

    popup

--- src/js/main/popup.js
+++ src/js/main/popup.js
@@ -1,46 +1,60 @@
 function modal() {
-  const modalBtn = [...document.querySelectorAll('.js-link-popup')]
-  const modalWrapper = document.querySelector('.js-popup')
-  modalWrapper.innerHTML = ''
+  // const modalBtn = [...document.querySelectorAll('.js-link-popup')]
+  // const modalWrapper = document.querySelector('.js-popup')
+  // modalWrapper.innerHTML = ''
 
-  setTimeout(() => {
-    // modalWrapper.innerHTML = ''
-    const firtSlideURL = './assets/popup/first-slide.html'
-    getModalContent(firtSlideURL)
-    modalWrapper.classList.add('is-active')
+  // setTimeout(() => {
+  //   // modalWrapper.innerHTML = ''
+  //   const firtSlideURL = './assets/popup/first-slide.html'
+  //   getModalContent(firtSlideURL)
+  //   modalWrapper.classList.add('is-active')
+
+  //   document.addEventListener('click', e => {
+  //     if (e.target.classList.contains('js-close-popup')) {
+  //       modalWrapper.classList.remove('is-active')
+  //       modalWrapper.innerHTML = ''
+  //     }
+  //   })
+  // }, 1500)
+
+  // modalBtn.forEach(function (element) {
+  //   element.addEventListener('click', function(event) {
+  //     // modalWrapper.innerHTML = ''
+  //     getModalContent(event.target.dataset.url)
+  //     modalWrapper.classList.add('is-active')
+
+  //     document.addEventListener('click', e => {
+  //       if (e.target.classList.contains('js-close-popup')) {
+  //         modalWrapper.classList.remove('is-active')
+  //         modalWrapper.innerHTML = ''
+  //       }
+  //    })
+  //   })
+  // })
+
+//   function getModalContent(url) {
+//     fetch(url)
+//       .then((response) => {
+//         return response.text();
+//       })
+//       .then((data) => {
+//         modalWrapper.innerHTML = data
+//       });
+//   }
+const modalBtn = [...document.querySelectorAll('.js-link-popup')]
+
+  modalBtn.forEach((el) => {
+    let link = el.dataset.popup
+    el.addEventListener('click', (e) => {
+      document.getElementById(link).classList.add('is-active')
+    })
 
     document.addEventListener('click', e => {
       if (e.target.classList.contains('js-close-popup')) {
-        modalWrapper.classList.remove('is-active')
-        modalWrapper.innerHTML = ''
+       document.getElementById(link).classList.remove('is-active')
       }
     })
-  }, 1500)
-
-  modalBtn.forEach(function (element) {
-    element.addEventListener('click', function(event) {
-      // modalWrapper.innerHTML = ''
-      getModalContent(event.target.dataset.url)
-      modalWrapper.classList.add('is-active')
-
-      document.addEventListener('click', e => {
-        if (e.target.classList.contains('js-close-popup')) {
-          modalWrapper.classList.remove('is-active')
-          modalWrapper.innerHTML = ''
-        }
-     })
-    })
   })
-
-  function getModalContent(url) {
-    fetch(url)
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        modalWrapper.innerHTML = data
-      });
-  }
 }
 
 modal()
